7847fcc126dddeca7bb481fce2b35086e50a320a
fernandog301 <fernandog301g@gmail.com>
2024-05-13 00:19:59 -0700

    test

--- src/app/Components/ContactComponents.tsx
+++ src/app/Components/ContactComponents.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ContactComponents() {
   return (
     
-    <section className='bg-neutral-700 text-white pb-8'>
+    <section className='bg-black text-white pb-8'>
                 <h2 className='text-center text-4xl md:text-6xl lg:text-7xl font-bold py-8'>Contact</h2>
                 <p className='text-center text-xl pb-4'>fgutierrez@codestack.co</p>
                 <p className='text-center text-xl pb-4'>Stockton, CA</p>
